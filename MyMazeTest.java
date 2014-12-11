6329106572a10e372687e09fda5f91ebc7b5d070
Skovy <spencer@skovistudios.com>
2014-12-11 13:44:31 -0500

    added test method stubs

--- MyMazeTest.java
+++ MyMazeTest.java
@@ -1,12 +1,53 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
-
+/**
+ * 
+ * @author Spencer Miskoviak
+ * @author Duncan Fox
+ * 
+ * CS2321 | Data Structures | Fall 2014
+ * 
+ * This program test the MyMaze structure
+ *
+ */
 public class MyMazeTest {
+	
+	@Test
+	public void generateMazeTest() {
+		fail("Not yet implemented");
+	}
 
 	@Test
-	public void test() {
+	public void solveMazeTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void toGraphTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void toArrayTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void startVertexTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void finishVertexTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void toStringTest() {
 		fail("Not yet implemented");
 	}
 
