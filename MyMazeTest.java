eb6c9fa501b3feb3fe6aca2bcdfaefc0980a355e
Skovy <spencer@skovistudios.com>
2014-12-11 14:05:28 -0500

    tests

--- MyMazeTest.java
+++ MyMazeTest.java
@@ -18,7 +18,83 @@ public class MyMazeTest {
 	
 	@Test
 	public void generateMazeTest() {
-		fail("Not yet implemented");
+		
+		MyMaze maze = new MyMaze();
+		
+		// normal maze test
+		int mazeSize = 10;
+		maze.generateMaze(mazeSize, mazeSize);
+		
+		// check vertices count
+		if(maze.toGraph().vertices().size() != (mazeSize * mazeSize)) {
+			fail(mazeSize + " x " + mazeSize + " maze should have " + (mazeSize * mazeSize) + " vertices");
+		}
+
+		// check start point
+		if(maze.startVertex() == null) {
+			fail("start vertex is not set after generating maze");
+		}
+		if(maze.startVertex().getElement().getX() != 0) {
+			fail("start vertex should be on the left side of the maze");
+		}
+		
+		// check end point
+		if(maze.finishVertex() == null) {
+			fail("finish vertex is not set after generating maze");
+		}
+		if(maze.finishVertex().getElement().getX() != mazeSize) {
+			fail("finish vertex should be on the right side of the maze");
+		}
+		
+		// small maze test
+		mazeSize = 1;
+		maze.generateMaze(mazeSize, mazeSize);
+		
+		// check vertices count
+		if(maze.toGraph().vertices().size() != (mazeSize * mazeSize)) {
+			fail(mazeSize + " x " + mazeSize + " maze should have " + (mazeSize * mazeSize) + " vertices");
+		}
+
+		// check start point
+		if(maze.startVertex() == null) {
+			fail("start vertex is not set after generating maze");
+		}
+		if(maze.startVertex().getElement().getX() != 0) {
+			fail("start vertex should be on the left side of the maze");
+		}
+		
+		// check end point
+		if(maze.finishVertex() == null) {
+			fail("finish vertex is not set after generating maze");
+		}
+		if(maze.finishVertex().getElement().getX() != mazeSize) {
+			fail("finish vertex should be on the right side of the maze");
+		}
+		
+		// small maze test
+		mazeSize = 2;
+		maze.generateMaze(mazeSize, mazeSize);
+		
+		// check vertices count
+		if(maze.toGraph().vertices().size() != (mazeSize * mazeSize)) {
+			fail(mazeSize + " x " + mazeSize + " maze should have " + (mazeSize * mazeSize) + " vertices");
+		}
+
+		// check start point
+		if(maze.startVertex() == null) {
+			fail("start vertex is not set after generating maze");
+		}
+		if(maze.startVertex().getElement().getX() != 0) {
+			fail("start vertex should be on the left side of the maze");
+		}
+		
+		// check end point
+		if(maze.finishVertex() == null) {
+			fail("finish vertex is not set after generating maze");
+		}
+		if(maze.finishVertex().getElement().getX() != mazeSize) {
+			fail("finish vertex should be on the right side of the maze");
+		}
 	}
 
 	@Test
@@ -33,7 +109,40 @@ public class MyMazeTest {
 
 	@Test
 	public void toArrayTest() {
-		fail("Not yet implemented");
+		
+		MyMaze maze = new MyMaze();
+		int mazeSize = 10;
+		maze.generateMaze(mazeSize, mazeSize);
+
+		// check array size
+		if(maze.toArray().length != mazeSize) {
+			fail("Array length is not " + mazeSize);
+		}
+		if(maze.toArray()[0].length != mazeSize) {
+			fail("Array[0] length is not " + mazeSize);
+		} 
+		
+		mazeSize = 1;
+		maze.generateMaze(mazeSize, mazeSize);
+
+		// check array size
+		if(maze.toArray().length != mazeSize) {
+			fail("Array length is not " + mazeSize);
+		}
+		if(maze.toArray()[0].length != mazeSize) {
+			fail("Array[0] length is not " + mazeSize);
+		}
+		
+		mazeSize = 2;
+		maze.generateMaze(mazeSize, mazeSize);
+
+		// check array size
+		if(maze.toArray().length != mazeSize) {
+			fail("Array length is not " + mazeSize);
+		}
+		if(maze.toArray()[0].length != mazeSize) {
+			fail("Array[0] length is not " + mazeSize);
+		}
 	}
 
 	@Test
