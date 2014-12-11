d37aea17353964759279dff59312de696f1e0e7c
Skovy <spencer@skovistudios.com>
2014-12-11 13:22:45 -0500

    added MyMaze & MyPair

---
+++ MyMaze.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+
+public class MyMaze implements Maze {
+
+	@Override
+	public void generateMaze(int rows, int columns) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Vertex> solveMaze() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Graph toGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex[][] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex startVertex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex finishVertex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String toString() {
+		return "";
+	}
+
+}
---
+++ MyPair.java
@@ -0,0 +1,31 @@
+
+public class MyPair implements Pair {
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setX(int newVal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setY(int newVal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String toString() {
+		return "";
+	}
+}
