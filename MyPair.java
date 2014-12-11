2da4b230bb5aacb13fedff06d92522d13122a4fb
dbfox <dbfox@mtu.edu>
2014-12-11 14:14:17 -0500

    Finished the MyVertex class

--- MyPair.java
+++ MyPair.java
@@ -1,31 +1,67 @@
 
+/**
+ * 
+ * @author Spencer Miskoviak
+ * @author Duncan Fox
+ * 
+ * CS2321 | Data Structures | Fall 2014
+ * 
+ * This program implements a stored pair of coordinates
+ *
+ */
 public class MyPair implements Pair {
 
+	private int x = 0;
+	private int y = 0;
+	
 	@Override
+	/**
+	 * Returns the x value of the (x,y) pair
+	 * 
+	 * @return The x axis position of the coordinate pair
+	 */
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	@Override
+	/**
+	 * Sets the x value of the (x,y) pair
+	 * 
+	 * @param newVal The x axis position of the coordinate pair
+	 */
 	public void setX(int newVal) {
-		// TODO Auto-generated method stub
-		
+		x = newVal;
 	}
 
 	@Override
+	/**
+	 * Returns the y value of the (x,y) pair
+	 * 
+	 * @return The y axis position of the coordinate pair
+	 */
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	@Override
+	/**
+	 * Sets the y value of the (x,y) pair
+	 * 
+	 * @param newVal The y axis position of the coordinate pair
+	 */
 	public void setY(int newVal) {
-		// TODO Auto-generated method stub
-		
+		y = newVal;
 	}
 
+	@Override
+	/**
+	 * Returns a string representation of the coordinate pair
+	 * 
+	 * @return A string containing the x and y axis positions
+	 * 		   of the coordinate pair
+	 */
 	public String toString() {
-		return "";
+		return "(" + x + "," + y + ")";
 	}
 }
--- MyVertex.java
+++ MyVertex.java
@@ -12,31 +12,60 @@ import java.util.ArrayList;
  */
 public class MyVertex implements Vertex {
 
+	ArrayList< Edge > edges = new ArrayList< Edge >();
+	ArrayList< Vertex > vertices = new ArrayList< Vertex >();
+	Pair xyCoord = null;
+	
 	@Override
+	/**
+	 * Returns the x,y coordinate pair that represents where the vertex
+	 * is located in the graph
+	 * 
+	 * @return The x,y pair that identifies the vertex's position
+	 */
 	public Pair getElement() {
-		// TODO Auto-generated method stub
-		return null;
+		return xyCoord;
 	}
 
 	@Override
+	/**
+	 * Sets the x,y coordinate pair that represents where the vertex
+	 * is located in the graph
+	 * 
+	 * @param e The x,y pair that identifies the vertex's position
+	 */
 	public void setElement(Pair e) {
-		// TODO Auto-generated method stub
-		
+		xyCoord = e;
 	}
 
 	@Override
+	/**
+	 * Returns a list of all edges that connect to the vertex
+	 * 
+	 * @return An ArrayList of all edges that connect to the vertex
+	 */
 	public ArrayList<Edge> incidentEdges() {
-		// TODO Auto-generated method stub
-		return null;
+		return edges;
 	}
 
 	@Override
+	/**
+	 * Returns a list of all vertices that share an edge with the vertex
+	 * 
+	 * @return An ArrayList of all vertices that share an edge with the vertex
+	 */
 	public ArrayList<Vertex> adjacentVertices() {
-		// TODO Auto-generated method stub
-		return null;
+		return vertices;
 	}
 
+	@Override
+	/**
+	 * Returns a string representation of the vertex, including the
+	 * coordinate pair that represents where it is located in a graph
+	 * 
+	 * @return A string representing the vertex and its position
+	 */
 	public String toString() {
-		return "";
+		return "<v" + xyCoord + ">";
 	}
 }
